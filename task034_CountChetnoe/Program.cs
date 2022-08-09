8f38e07f4c2bc55a9a94321fe4e46dd8fc7f44b6
Svetlana <brodlik@me.com>
2022-08-09 15:29:15 +0300

    Задача по поиску и подсчету четных чисев в массиве

---
+++ task034_CountChetnoe/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int[] array = FillArray(4, 100, 1000);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+int countChetnoe = 0; 
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]%2==0)
+    {
+         countChetnoe ++;
+    }
+}
+Console.Write($"Количество четных чисел = {countChetnoe}");
